ce752890e5c762f663d668cef438fdff98ff8f0f
protapsaha1 <webprotap09@gmail.com>
2023-03-08 22:56:49 +0600

    del button and AC button

--- js/calculator.js
+++ js/calculator.js
@@ -3,9 +3,37 @@ const getElementText = (id, value) =>{
     return getId;
 };
 const number = (id) =>{
-    const getId = document.getElementById(id);
-    return getId;
+    const getId = document.getElementById(id).innerText;
+    const getString = parseFloat(getId);
+    return getString;
 };
-const  deleteLastNum =() =>{
-    
-};
+document.getElementById('calculator').addEventListener('click', function(event){
+    const calculatorBtn = event.target.innerText;
+    const inputField = document.getElementById('input-fields');
+    const fieldValue = inputField.value;
+
+    if(isNaN(calculatorBtn)){
+        if(calculatorBtn === 'AC'){
+            inputField.value = '';
+        }
+        else if(calculatorBtn === 'Del'){
+            const del = fieldValue.split('');
+            del.pop();
+            const lastDigit = del.join('');
+            getElementText('input-fields',lastDigit);
+        }
+    }
+    else{
+        const num = fieldValue + calculatorBtn;
+        inputField.value = num;
+    };
+});
+
+// const multiple_pi =()=>{
+
+//     if(calculatorBtn === 'pi'){
+//         const pi = 3.14;
+//         // const piSum = inputField.value * pi ;
+//         getElementText('input-fields', pi);
+//     }
+// }
