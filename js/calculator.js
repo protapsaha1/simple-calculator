b3831aa59cb7cb179b7473921c65b3df93e78ec9
protapsaha1 <webprotap09@gmail.com>
2023-03-08 22:04:22 +0600

    push html code

---
+++ js/calculator.js
@@ -0,0 +1,11 @@
+const getElementText = (id, value) =>{
+    const getId = document.getElementById(id).value = value;
+    return getId;
+};
+const number = (id) =>{
+    const getId = document.getElementById(id);
+    return getId;
+};
+const  deleteLastNum =() =>{
+    
+};
